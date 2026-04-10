33bbb3300393e043157aaedde915c5b28f7dc02a
antonylrds <antonylrds@gmail.com>
2026-04-09 23:52:32 -0300

    Jump

--- scripts/character_body_2d.gd
+++ scripts/character_body_2d.gd
@@ -7,23 +7,31 @@ const JUMP_VELOCITY = -400.0
 func _ready():
 	$AnimatedSprite2D.play("idle")
 
+func should_run():
+	if is_on_floor():
+		$AnimatedSprite2D.play("running")
+		
 
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
+		if velocity.y < 0:
+			$AnimatedSprite2D.play("jump")
+		elif velocity.y >= 0:
+			$AnimatedSprite2D.play("fall")
 		velocity += get_gravity() * delta
-		
-	print(is_on_floor())
-
 	# Handle jump.
-	if Input.is_action_pressed("left"):
-		$AnimatedSprite2D.play("running")
-		$AnimatedSprite2D.flip_h = true
-		#velocity.y = JUMP_VELOCITY
+	elif Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+	elif Input.is_action_pressed("left"):
+		should_run()
 	elif Input.is_action_pressed("right"):
-		$AnimatedSprite2D.play("running")
+		should_run()
 		$AnimatedSprite2D.flip_h = false
 	else:
 		$AnimatedSprite2D.play("idle")
+	
+	if velocity.x != 0:
+		$AnimatedSprite2D.flip_h = velocity.x < 0
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
